b38fa2001617867aaa7c86160be114e92d24a80a
AlvaroSantosCalvo <aalvar.s.c@gmail.com>
2026-01-26 12:52:31 +0100

    Separado el css

--- nebriacademy-frontend/src/App.jsx
+++ nebriacademy-frontend/src/App.jsx
@@ -1,5 +1,11 @@
-import AppRouter from './router/AppRouter.jsx'
-import './styles.css'
+import AppRouter from './router/AppRouter.jsx';
+import './styles/general.css';
+import './styles/Header.css';
+import './styles/Nav.css';
+import './styles/Footer.css';
+import './styles/Login.css';
+import './styles/Perfil.css';
+import './styles/TodosCursos.css';
 
 function App() {
 
