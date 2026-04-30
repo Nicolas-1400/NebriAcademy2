9f8a0e0d874fcac02da59185719b15b58cf88505
Nicolás-1400 <nicolasgarciasampedrodocampo@gmail.com>
2026-04-30 10:42:45 +0200

    Cambio mínimo

--- nebriacademy-frontend/src/pages/Ayuda.jsx
+++ nebriacademy-frontend/src/pages/Ayuda.jsx
@@ -1,6 +1,7 @@
 import Nav from "../components/Nav"
 import AyudaGrid from "../components/AyudaGrid"
 import Footer from "../components/Footer"
+
 function Ayuda() {
   return (
     <div>
--- nebriacademy-frontend/src/pages/MiEspacio.jsx
+++ nebriacademy-frontend/src/pages/MiEspacio.jsx
@@ -1,7 +1,6 @@
 import Nav from "../components/Nav";
 import MiEspacioGrid from "../components/MiEspacioGrid";
 import Footer from "../components/Footer";
-import "../styles/HomeEspacio.css";
 
 function MiEspacio() {
   return (
